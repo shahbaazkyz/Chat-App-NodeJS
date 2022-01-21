a062fd38505c574fed2ad722e3ce04c3fdc0ff2b
Shahbaazkyz <Shahbaazkyz@gmail.com>
2022-01-22 01:23:00 +0500

    ✅ Removing user util!

--- src/utils/users.js
+++ src/utils/users.js
@@ -29,3 +29,12 @@ const addUser = ({ id, username, room }) => {
   users.push(user);
   return { user };
 };
+
+
+//* Removing user.
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
